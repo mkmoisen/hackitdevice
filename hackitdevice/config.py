52bb11a8927ebd4cb88db928d9b5c63ee3b05367
Matthew Moisen <mmoisen@cisco.com>
2016-04-01 00:29:33 -0700

    changed to dev server

--- hackitdevice/config.py
+++ hackitdevice/config.py
@@ -1,3 +1,3 @@
 __author__ = 'mmoisen'
-FRAMEWORK_URL = 'http://10.154.241.186:5000'
-FRAMEWORK_PUSH = '/hackit/device/payload/post/'
+FRAMEWORK_URL = 'https://sbpmed-dev01'
+FRAMEWORK_PUSH = '/hackit/device/payload/'
