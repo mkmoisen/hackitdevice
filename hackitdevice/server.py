524afa3fe456a88146277cebb87cd90feb0dc874
Matthew Moisen <mmoisen@cisco.com>
2016-04-01 05:27:32 -0700

    initial server

--- hackitdevice/server.py
+++ hackitdevice/server.py
@@ -3,9 +3,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from models import Heater
+
 @app.route("/home")
 def home():
     return "haii"
 
+@app.route("/lamp/")
+def lamp():
+    relay = Heater(17)
+    if relay.is_on:
+        relay.turn_off()
+    else:
+        relay.turn_on()
+
 def start():
-    app.run(host='169.254.0.2', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
