1b1b0de83de627e3c31a5889de200884b1a0478f
Matthew Moisen <mmoisen@cisco.com>
2016-04-01 08:38:58 -0700

    register ip before calling run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 __author__ = 'mmoisen'
 #from hackitdevice.controller import start
 from hackitdevice.controller import start
+import hackitdevice.register_ip
 if __name__ == '__main__':
     start()
